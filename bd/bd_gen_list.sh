268568b9b390beef7c324bd922ea7adfe0e624ef
Yoann Sculo <yoann.sculo@gmail.com>
2012-07-05 22:38:56 +0200

    add bootstrap and update css

--- bd/bd_gen_list.sh
+++ bd/bd_gen_list.sh
@@ -3,8 +3,14 @@
 OUTPUT=./liste.html
 
 echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" > $OUTPUT
-echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"fr-FR\"><head><meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\"></head><body>" >> $OUTPUT
-echo "<table>" >> $OUTPUT
+echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"fr-FR\">"
+echo "<head>"
+echo "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">"
+echo "<link href=\"bootstrap/css/bootstrap.css\" rel=\"stylesheet\">"
+echo "</head><body>" >> $OUTPUT
+echo "<table class=\"table table-bordered\">" >> $OUTPUT
+
+echo "<thead><tr><th>Titre</th><th>Auteurs</th><th>ISBN13</th></tr></thead>" >> $OUTPUT
 
 tail -n +2 list.csv | while read line
 do
